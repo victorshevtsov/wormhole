f1d922ca93c9eeb24b275071cce14ce452f41ef9
Victor Shevtsov <victorshevtsov@gmail.com>
2022-01-20 18:49:33 +0100

    fix copySafecoinWasm.js

--- sdk/js/scripts/copySafecoinWasm.js
+++ sdk/js/scripts/copySafecoinWasm.js
@@ -1,65 +1,67 @@
 const fs = require("fs");
-fs.copyFileSync(
-  "src/safecoin/core/bridge_bg.wasm",
-  "lib/safecoin/core/bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/core-node/bridge_bg.wasm",
-  "lib/safecoin/core-node/bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/core/bridge_bg.wasm.d.ts",
-  "lib/safecoin/core/bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/core-node/bridge_bg.wasm.d.ts",
-  "lib/safecoin/core-node/bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/nft/nft_bridge_bg.wasm",
-  "lib/safecoin/nft/nft_bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/nft-node/nft_bridge_bg.wasm",
-  "lib/safecoin/nft-node/nft_bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/nft/nft_bridge_bg.wasm.d.ts",
-  "lib/safecoin/nft/nft_bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/nft-node/nft_bridge_bg.wasm.d.ts",
-  "lib/safecoin/nft-node/nft_bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/token/token_bridge_bg.wasm",
-  "lib/safecoin/token/token_bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/token-node/token_bridge_bg.wasm",
-  "lib/safecoin/token-node/token_bridge_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/token/token_bridge_bg.wasm.d.ts",
-  "lib/safecoin/token/token_bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/token-node/token_bridge_bg.wasm.d.ts",
-  "lib/safecoin/token-node/token_bridge_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/migration/wormhole_migration_bg.wasm",
-  "lib/safecoin/migration/wormhole_migration_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/migration-node/wormhole_migration_bg.wasm",
-  "lib/safecoin/migration-node/wormhole_migration_bg.wasm"
-);
-fs.copyFileSync(
-  "src/safecoin/migration/wormhole_migration_bg.wasm.d.ts",
-  "lib/safecoin/migration/wormhole_migration_bg.wasm.d.ts"
-);
-fs.copyFileSync(
-  "src/safecoin/migration-node/wormhole_migration_bg.wasm.d.ts",
-  "lib/safecoin/migration-node/wormhole_migration_bg.wasm.d.ts"
-);
+["lib/esm", "lib/cjs"].forEach((buildPath) => {
+  fs.copyFileSync(
+    `src/safecoin/core/bridge_bg.wasm`,
+    `${buildPath}/safecoin/core/bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/core-node/bridge_bg.wasm`,
+    `${buildPath}/safecoin/core-node/bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/core/bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/core/bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/core-node/bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/core-node/bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/nft/nft_bridge_bg.wasm`,
+    `${buildPath}/safecoin/nft/nft_bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/nft-node/nft_bridge_bg.wasm`,
+    `${buildPath}/safecoin/nft-node/nft_bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/nft/nft_bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/nft/nft_bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/nft-node/nft_bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/nft-node/nft_bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/token/token_bridge_bg.wasm`,
+    `${buildPath}/safecoin/token/token_bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/token-node/token_bridge_bg.wasm`,
+    `${buildPath}/safecoin/token-node/token_bridge_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/token/token_bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/token/token_bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/token-node/token_bridge_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/token-node/token_bridge_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/migration/wormhole_migration_bg.wasm`,
+    `${buildPath}/safecoin/migration/wormhole_migration_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/migration-node/wormhole_migration_bg.wasm`,
+    `${buildPath}/safecoin/migration-node/wormhole_migration_bg.wasm`
+  );
+  fs.copyFileSync(
+    `src/safecoin/migration/wormhole_migration_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/migration/wormhole_migration_bg.wasm.d.ts`
+  );
+  fs.copyFileSync(
+    `src/safecoin/migration-node/wormhole_migration_bg.wasm.d.ts`,
+    `${buildPath}/safecoin/migration-node/wormhole_migration_bg.wasm.d.ts`
+  );
+});
